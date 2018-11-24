e3216082760d6231107872472b3a03117b0ceb60
Ddone <mynameisthenekit@gmail.com>
2018-11-24 03:57:52 +0200

    olololo

--- Server/main.py
+++ Server/main.py
@@ -9,17 +9,29 @@ port = os.getenv("PORT",1)
 
 us = api.User("XoMute")
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write("say something")
+
+    def set_default_headers(self, *args, **kwargs):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+class MainHandler(BaseHandler):
     def get(self):
         self.write("Hello, world")
 
-class DialogueChanged(tornado.web.RequestHandler):
+class DialogueChanged(BaseHandler):
     def get(self):
         self.write("Hello, world")
     def post(self):
         print(sels.request.body)
         self.write("You posted")
 
+class DataHandler(BaseHandler):
+    def get(self):
+        self.write("Jopa")
 
 def make_app():
     return tornado.web.Application([
