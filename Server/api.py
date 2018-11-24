bf7261ffbd520e0d01ba46d6b561aa2a88dbb678
Ddone <mynameisthenekit@gmail.com>
2018-11-24 03:34:14 +0200

    lolkekcheburek

--- Server/api.py
+++ Server/api.py
@@ -1,8 +1,80 @@
+from telethon import TelegramClient, sync
+from telethon.tl.patched import Message
+from string_session import StringSession
+import os
 
-
-
+API_ID = "445674"
+API_HASH =" 921ce6d709ed0b6f8b487747c18b50e6"
+TELEGRAM_APPID=473887
+TELEGRAM_APIHASH="aef057abbbd06dbae724a7e7a8dca9ef"
 
 
 
 class User:
+    client = 0;
+    dataloaded = False
+    dataready = False
+    data = {}
+    chatter = None
+    def __init__(self, uname ):
+        if uname in os.listdir("./sessions"):
+            with open("./sessions/"+uname,"r") as i:
+                session = i.read()
+                self.client = TelegramClient(StringSession(session), TELEGRAM_APPID, TELEGRAM_APIHASH)
+                self.client.start()
+        else:
+            session = StringSession()
+            self.client = TelegramClient(session,TELEGRAM_APPID,TELEGRAM_APIHASH)
+            self.client.start()
+            with open("./sessions/"+uname,"w") as o:
+                o.write(session.save())
+    def dialogue_changed(self, user): 
+        dataloaded = False
+        self.chatter = user
+        if user not in self.data.keys():
+            self.data[user] = []
+            for mes in self.client.iter_messages(self.client.get_entity(user),limit = 300):
+                if not type(mes) is Message:
+                    #print(type(mes))
+                    continue
+                text = mes.text
+                out = mes.out
+                date = mes.date
+                friend = user
+                id = mes.id
+                self.data[user].append({  "text":text,
+                    "in":not out,
+                    "time":date,
+                    "chat":user,
+                    "id":id})
+
+        else:
+            temp = []
+            for mes in self.client.iter_messages(self.client.get_entity(user),limit = 300,min_id=(self.data[user][0] and self.data[user][0]["id"] or 0) ):
+                if not type(mes) is Message:
+                    #print(type(mes))
+                    continue
+                text = mes.text
+                out = mes.out
+                date = mes.date
+                friend = user
+                id = mes.id
+                temp.append({  "text":text,
+                    "in":not out,
+                    "time":date,
+                    "chat":user,
+                    "id":id})
+            self.data[user] = temp + self.data[user]
+                         
+
+        dataloaded = True
+        print(self.data[user][::-1])
+    
+        
+#xo = User("XoMute")
+#xo.dialogue_changed("deedone")
+#input()
+#xo.dialogue_changed("deedone")
+
+
 
--- Server/main.py
+++ Server/main.py
@@ -1,13 +1,28 @@
 import tornado.ioloop
 import tornado.web
 
+import api
+
+
+us = api.User("XoMute")
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class DialogueChanged(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+    def post(self):
+        print(sels.request.body)
+        self.write("You posted")
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/dialogue",DialogueChanged)
+
     ])
 
 if __name__ == "__main__":
